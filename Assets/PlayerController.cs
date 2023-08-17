471f868ac4cd237ba2c62b6d905e689509d42601
dmy0511 <dmy0511love@gmail.com>
2023-08-17 12:42:03 +0900

    08/17

---
+++ Assets/PlayerController.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    public float _speed = 10.0f;
+
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.W))
+            transform.position += Vector3.forward * Time.deltaTime * _speed;
+        
+        if (Input.GetKey(KeyCode.S))
+            transform.position += Vector3.back * Time.deltaTime * _speed;
+        
+        if (Input.GetKey(KeyCode.D))
+            transform.position += Vector3.right * Time.deltaTime * _speed;
+        
+        if (Input.GetKey(KeyCode.A))
+            transform.position += Vector3.left * Time.deltaTime * _speed;
+    }
+}
